0f6c2b21734f7af20df6635e50612c2679d55c6c
tfarida <37550133+tfarida@users.noreply.github.com>
2018-06-02 09:00:19 -0500

    lab04_childProcess.js : remove unused function

--- Homework 04 - NodeJS 03/lab04_childProcess.js
+++ Homework 04 - NodeJS 03/lab04_childProcess.js
@@ -2,20 +2,6 @@
 const fs = require('fs');
 var path = require('path');
 
-const readingFile = (path_file)=> {
-    console.log('path_file: '+path_file);   
-    //var src = fs.createReadStream(path.join(__dirname, 'images', './image_big_padang_IMG_9800.JPG'));
-    var src = fs.createReadStream(path_file);
-    src.on('data', function(data){
-        process.send(data);
-    });
-
-    src.on('end', function(){
-        process.send('END');
-    })
-
-};
-
 process.on('message', (msg) => {
     console.log('msg: '+msg);
     var path_file = msg;
