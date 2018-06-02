4230bd2a47241f4ec316779d29a914fb5d418613
tfarida <37550133+tfarida@users.noreply.github.com>
2018-06-02 08:20:38 -0500

    done prob Reactive server with child process. to test: - run lab04_reactiveServer.js, - go to http://localhost:1337/?url=pathToMyFile/file.txt

--- Homework 04 - NodeJS 03/lab04_reactiveServer.js
+++ Homework 04 - NodeJS 03/lab04_reactiveServer.js
@@ -1,16 +1,34 @@
+//lab04 prob01: create a Reactive HTTP server that serves file whe url query parameter is passed with the request.
+//the file path is provided in the URL like this
+//http://localhost:4000/?url=path/to/my/file.txt
+//your logic in reading file should be in a separate child process. 
+//hint: main and child must communicate, so use fork
+
 const {Subject} = require('rxjs');
 const subject  = new Subject();
 const http = require('http');
 const {fork} = require('child_process');
 
 function serveFile(reqres){
-    var path_to_myFile = reqres.req.url.path;
+    //the file path is provided in the URL like this
+    //http://localhost:4000/?url=path/to/my/file.txt
+    var url = reqres.req.url;
+    console.log("url: "+url);
+    var path_file = url.substr(6,url.length);
+    console.log("urls path_file: "+path_file);
 
+    //your logic in reading file should be in a separate child process.
     const childProcess = fork('lab04_childProcess.js');
-    // childProcess.send('start');
-    // childProcess.on('message', data => {
-    //    reqres.res.write(data);
-    // })
+    childProcess.send(path_file);
+    childProcess.on('message', data => {
+        if(data != 'END'){
+            reqres.res.write(data);
+        }
+        else{
+            reqres.res.end();
+        }
+        
+    });
 }
 
 subject.subscribe(serveFile)
